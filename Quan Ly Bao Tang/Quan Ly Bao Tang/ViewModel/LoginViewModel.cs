01034cf86bd6f80f9d73067bf094f319bbb5d2ba
PC <PC@DESKTOP-LT1I4U7>
2018-08-22 17:30:45 +0700

    thay

---
+++ Quan Ly Bao Tang/Quan Ly Bao Tang/ViewModel/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quan_Ly_Bao_Tang.View;
+
+namespace Quan_Ly_Bao_Tang.ViewModel
+{
+    class LoginViewModel
+    {
+        public LoginViewModel()
+        {
+            
+        }
+    }
+}
